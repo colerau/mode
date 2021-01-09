22798dba4238bd484ca878edbc078a2bd1d2a4b6
Cole Rau <colevrau@gmail.com>
2021-01-08 21:51:28 -0800

    successfully got user's input into an array

--- algo.js
+++ algo.js
@@ -12,5 +12,19 @@ let array = [-2, 9, 9, 9, -2, -2, -2, -2, -2, -1, 0, 3, 1, 1, 1, 9, 10, 11, 11, 
 
 let result = findMode(array)
 
-console.log(result)
+// console.log(result)
 
+let userInput = document.getElementById("user-array").value 
+
+let form = document.getElementById("form")
+
+form.addEventListener("submit", e => {
+  e.preventDefault()
+  let userInput = document.getElementById("user-array").value.trim()
+  let array = []
+  debugger
+  for (let i = 0; i < userInput.length; i++) {
+    array.push(userInput[i])
+  }
+  console.log(array)
+})
