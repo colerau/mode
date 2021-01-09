2659cde1cda88579226969bd69d2dcb540c071d7
Cole Rau <colevrau@gmail.com>
2021-01-08 22:25:38 -0800

    mode finding algorithm works

--- algo.js
+++ algo.js
@@ -1,10 +1,29 @@
 function findMode(array) {
-  // create object where the keys are each unique number in the array and the values are the amount of times the number appears in the array
+  // create object where the keys are each unique number in the array and the values are the amount of times each unique number appears in the array
 
   let object = {}
   for (let i = 0; i < array.length; i++) {
-    
+    if (object[array[i]]) {
+      // increment existing key's value
+      object[array[i]] += 1
+    } else {
+      // make a new key and set value to 1
+      object[array[i]] = 1
+    }
   }
+
+  let biggestValue = -1
+  let biggestValuesKey = -1
+
+  // finding the biggest value and its corresponding key
+  Object.keys(object).forEach(key => {
+    let value = object[key]
+    if (value > biggestValue) {
+      biggestValue = value
+      biggestValuesKey = key
+    }
+  })
+  return biggestValuesKey
 }
 
 // mode is visibly 11
@@ -22,9 +41,9 @@ form.addEventListener("submit", e => {
   e.preventDefault()
   let userInput = document.getElementById("user-array").value.trim()
   let array = []
-  debugger
   for (let i = 0; i < userInput.length; i++) {
     array.push(userInput[i])
   }
-  console.log(array)
+  let mode = findMode(array)
+  console.log(mode)
 })
