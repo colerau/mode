0503f62f5acc4420ecb40ee5e9aed51ccac6257e
Cole Rau <colevrau@gmail.com>
2021-01-08 20:49:48 -0800

    set up algo.js file and started findMode function

--- algo.js
+++ algo.js
@@ -1,1 +1,15 @@
-// test
+function findMode(array) {
+  // create object where the keys are each unique number in the array and the values are the amount of times the number appears in the array
+
+  let object = {}
+  for (let i = 0; i < array.length; i++) {
+    
+  }
+}
+
+// mode is visibly 11
+let array = [-2, 9, 9, 9, -2, -2, -2, -2, -2, -1, 0, 3, 1, 1, 1, 9, 10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11]
+
+let result = findMode(array)
+
+console.log(result)
